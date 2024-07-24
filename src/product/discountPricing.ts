68702b9cb41258248094adf542cfbdc46002c2b6
Yuri Zalvov <yzalvov@gmail.com>
2024-07-24 18:43:59 +0200

    Higher discount prevails

--- src/product/discountPricing.ts
+++ src/product/discountPricing.ts
@@ -4,6 +4,8 @@ import { getNewsletterDiscountParams } from './utils'
 
 const titlePriceDiv = document.getElementById('title-price')
 const strikedSpan = titlePriceDiv?.querySelector('span')
+const titlePriceDivMob = document.getElementById('title-price-mob')
+const strikedSpanMob = titlePriceDivMob?.querySelector('span')
 const yearBundleDiv = document.getElementById('year-bundle-price')
 const monthBundleDiv = document.getElementById('month-bundle-price')
 
@@ -29,71 +31,79 @@ function parsePageCurrentPrice() {
   }
 }
 
+function updateBundlePriceHtml(oldPrice, newPrice, div) {
+  if (div) {
+    const originalText = div.textContent
+    if (originalText) {
+      div.innerHTML = originalText.replace(
+        /\$[0-9,.]+/,
+        `<span class="text-style-strikethrough text-color-neutral-82 text-weight-normal">$${oldPrice.toFixed(
+          2
+        )}</span> $${newPrice.toFixed(2)}`
+      )
+    }
+  }
+}
+
 function upTitlePrice(strikedAfter, currentAfter) {
-  if (!titlePriceDiv) return
-  if (strikedSpan) {
-    strikedSpan.textContent = `$${strikedAfter.toFixed(2)}`
+  if (titlePriceDiv) {
+    if (strikedSpan) {
+      strikedSpan.textContent = `$${strikedAfter.toFixed(2)}`
+    }
+    titlePriceDiv.childNodes[1].nodeValue = ` $${currentAfter.toFixed(2)}`
   }
-  titlePriceDiv.childNodes[1].nodeValue = ` $${currentAfter.toFixed(2)}`
+  if (titlePriceDivMob) {
+    if (strikedSpanMob) {
+      strikedSpanMob.textContent = `$${strikedAfter.toFixed(2)}`
+    }
+    titlePriceDivMob.childNodes[1].nodeValue = ` $${currentAfter.toFixed(2)}`
+  }
 }
 
-function upYearBundlePrice(oldPrice, newPrice) {
-  if (yearBundleDiv) {
-    yearBundleDiv.innerHTML = `1-Year bundle - <span class="text-style-strikethrough text-color-neutral-82 text-weight-normal">$${oldPrice.toFixed(
-      2
-    )}</span> $${newPrice.toFixed(2)}`
-  }
-}
+function applyDiscount(before, value, isPercentage = false) {
+  const titlePrice = before.titleCurrentPrice
+  const yearPrice = before.yearBundlePrice
+  const monthPrice = before.monthBundlePrice
 
-function upMonthBundlePrice(oldPrice, newPrice) {
-  if (monthBundleDiv) {
-    monthBundleDiv.innerHTML = `1-Month bundle - <span class="text-style-strikethrough text-color-neutral-82 text-weight-normal">$${oldPrice.toFixed(
-      2
-    )}</span> $${newPrice.toFixed(2)}`
+  if (isPercentage) {
+    upTitlePrice(titlePrice, titlePrice * value)
+    updateBundlePriceHtml(yearPrice, yearPrice * value, yearBundleDiv)
+    updateBundlePriceHtml(monthPrice, monthPrice * value, monthBundleDiv)
+  } else {
+    upTitlePrice(titlePrice, titlePrice - value)
+    updateBundlePriceHtml(yearPrice, yearPrice - value, yearBundleDiv)
+    updateBundlePriceHtml(monthPrice, monthPrice - value, monthBundleDiv)
   }
 }
 
 export function discountPricing() {
   const before = parsePageCurrentPrice()
-  const newsletterDiscountParams = getNewsletterDiscountParams()
   const rewardful = getCookie<RewardfulBrowserCookie>('rewardful.referral')
+  const newsletterParams = getNewsletterDiscountParams()
+  const newsletterPercentOff = Number(
+    new URLSearchParams(newsletterParams || '').get('percentOff') || 0
+  )
+  const rewardfulPercentOff = rewardful?.coupon?.percent_off || 0
+  const rewardfulAmountOff = rewardful?.coupon?.amount_off || 0
 
-  let amountOff = 0
-  let percentOff = 0
-  if (newsletterDiscountParams) {
-    const params = new URLSearchParams(newsletterDiscountParams)
-    percentOff = Number(params.get('percentOff') || 0)
-  } else if (rewardful?.coupon) {
-    if (rewardful.coupon.amount_off) {
-      amountOff = rewardful.coupon.amount_off
-    } else if (rewardful.coupon.percent_off) {
-      percentOff = rewardful.coupon.percent_off
-    }
-  }
+  const yearPrice = before.yearBundlePrice
+  const newsletterDiscount = yearPrice * (newsletterPercentOff / 100)
+  const rewardfulDiscount = yearPrice * (rewardfulPercentOff / 100)
 
-  if (amountOff) {
-    upTitlePrice(before.titleCurrentPrice, before.titleCurrentPrice - amountOff)
-    upYearBundlePrice(
-      before.yearBundlePrice,
-      before.yearBundlePrice - amountOff
-    )
-    upMonthBundlePrice(
-      before.monthBundlePrice,
-      before.monthBundlePrice - amountOff
-    )
-  } else if (percentOff) {
-    upTitlePrice(
-      before.titleCurrentPrice,
-      before.titleCurrentPrice * (1 - percentOff / 100)
-    )
-    upYearBundlePrice(
-      before.yearBundlePrice,
-      before.yearBundlePrice * (1 - percentOff / 100)
-    )
-    upMonthBundlePrice(
-      before.monthBundlePrice,
-      before.monthBundlePrice * (1 - percentOff / 100)
-    )
+  const maxDiscount = Math.max(
+    newsletterDiscount,
+    rewardfulDiscount,
+    rewardfulAmountOff
+  )
+
+  if (maxDiscount === 0) return
+
+  if (rewardfulAmountOff === maxDiscount) {
+    applyDiscount(before, rewardfulAmountOff)
+  } else {
+    const discountMultiplier =
+      1 - Math.max(newsletterPercentOff, rewardfulPercentOff) / 100
+    applyDiscount(before, discountMultiplier, true)
   }
 }
 
