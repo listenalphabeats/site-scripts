c0bb80e29ae0bed46ea2bd701154f89c64df50f5
Yuri Zalvov <yzalvov@gmail.com>
2024-08-22 18:58:40 +0200

    Scope safeDecodeURIComponent

--- src/utils/safeDecodeURIComponent.ts
+++ src/utils/safeDecodeURIComponent.ts
@@ -1,12 +1,12 @@
 export function safeDecodeURIComponent(str: string) {
-  return isURLEncoded(str) ? decodeURIComponent(str) : str
-}
-
-function isURLEncoded(str: string) {
-  try {
-    const decodedStr = decodeURIComponent(str)
-    return encodeURIComponent(decodedStr) !== str
-  } catch (e) {
-    return false
+  function isURLEncoded(str: string) {
+    try {
+      const decodedStr = decodeURIComponent(str)
+      return encodeURIComponent(decodedStr) !== str
+    } catch (e) {
+      return false
+    }
   }
+
+  return isURLEncoded(str) ? decodeURIComponent(str) : str
 }
