99418293023e01de2473d345f6f47559a38be193
Yuri Zalvov <yzalvov@gmail.com>
2024-12-17 20:54:28 +0100

    Deprecate handleCartWithDiscount, getNewsletterDiscountParams

--- src/product-page/handleCartWithDiscount.ts
+++ src/product-page/handleCartWithDiscount.ts
@@ -7,6 +7,7 @@ import {
   redirectToPartnerPage,
 } from '../utils'
 
+/** @deprecated */
 export function handleCartWithDiscount() {
   const partner = getPartnerNameInStorage()
   if (partner) {
--- src/utils/getNewsletterDiscountParams.ts
+++ src/utils/getNewsletterDiscountParams.ts
@@ -1,5 +1,7 @@
+/** @deprecated */
 export const NEWSLETTER_DISCOUNT_PARAMS_STORAGE_ID = 'newsletterDiscountParams'
 
+/** @deprecated */
 export function getNewsletterDiscountParams() {
   return localStorage.getItem(NEWSLETTER_DISCOUNT_PARAMS_STORAGE_ID)
 }
