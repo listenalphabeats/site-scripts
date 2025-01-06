d39e2fe289906aabe9bfe86b65b1a2d9c6d25927
Yuri Zalvov <yzalvov@gmail.com>
2025-01-06 13:30:42 +0100

    Up CES offer values

--- src/offers.ts
+++ src/offers.ts
@@ -3,15 +3,15 @@ import { isStaging } from './utils'
 export function getMuseCESOffer() {
   return {
     discountName: 'Exclusive CES Discount',
-    couponId: isStaging() ? 'EV1esVhd' : 'CmUrEbFf',
-    amountOff: 200,
+    couponId: isStaging() ? 'BXkB18mL' : 'WSeHfM47',
+    amountOff: 100,
   }
 }
 
 export function getBrainbitCESOffer() {
   return {
     discountName: 'Exclusive CES Discount',
-    couponId: isStaging() ? '1P2r7jG5' : 'DhqopqfN',
-    amountOff: 299,
+    couponId: isStaging() ? 'Xos0oOEa' : 'A5K0hpO5',
+    amountOff: 200,
   }
 }
