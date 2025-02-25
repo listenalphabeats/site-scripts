a634b5a0c916a1ec53e77bb6def8e8e1271424c7
Yuri Zalvov <yzalvov@gmail.com>
2025-02-25 15:55:16 +0000

    Extend parsePriceValue

--- src/product-page/utils/parsePriceValue.ts
+++ src/product-page/utils/parsePriceValue.ts
@@ -2,6 +2,7 @@ export function parsePriceValue(priceElement: Element | null) {
   if (!priceElement) return
   const priceString = priceElement.textContent
   if (!priceString) return
-  const value = priceString.replace('$', '')
-  return Number(value)
+  const match = priceString.match(/\$(\d+(?:\.\d{2})?)/)
+  if (!match) return
+  return Number(match[1])
 }
