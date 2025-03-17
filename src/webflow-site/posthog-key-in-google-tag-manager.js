0867d87b839637c403fb8648c3826425848d047c
Yuri Zalvov <yzalvov@gmail.com>
2025-03-17 15:37:08 +0100

    Save posthog-key-in-google-tag-manager

---
+++ src/webflow-site/posthog-key-in-google-tag-manager.js
@@ -0,0 +1,5 @@
+function() {
+  return window.location.host === 'staging-alphabeats.webflow.io'
+    ? 'phc_QwumEXbEtahkrs2hRm1J9KRDusNZcOrRSAYN7EOdBQ6'
+    : 'phc_ScvzJrqAuKQ1ufjL33gMUdMOAZEUni9NQsHzgVs97nP'
+}
