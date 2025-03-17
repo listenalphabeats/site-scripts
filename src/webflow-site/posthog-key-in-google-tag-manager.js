7d0d40cd3da2d87eca06dcc02a665b71316a941e
Yuri Zalvov <yzalvov@gmail.com>
2025-03-17 15:38:57 +0100

    Add description

--- src/webflow-site/posthog-key-in-google-tag-manager.js
+++ src/webflow-site/posthog-key-in-google-tag-manager.js
@@ -1,3 +1,4 @@
+/** Being used in the Google Tag Manager as `PostHog API Key dynamic` */
 function() {
   return window.location.host === 'staging-alphabeats.webflow.io'
     ? 'phc_QwumEXbEtahkrs2hRm1J9KRDusNZcOrRSAYN7EOdBQ6'
